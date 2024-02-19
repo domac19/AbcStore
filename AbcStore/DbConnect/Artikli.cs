73dcbdc675aa22d23a03702587187188414c859f
Domagoj Licitar <licitar.domagoj@gmail.com>
2024-02-19 18:15:08 +0100

    Added DbContext to project

---
+++ AbcStore/DbConnect/Artikli.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+// Code scaffolded by EF Core assumes nullable reference types (NRTs) are not used or disabled.
+// If you have enabled NRTs for your project, then un-comment the following line:
+// #nullable disable
+
+namespace AbcStore.DbConnect
+{
+    public partial class Artikli
+    {
+        public int Id { get; set; }
+        public string Naziv { get; set; }
+        public string Kategorija { get; set; }
+        public string Cijena { get; set; }
+    }
+}

ef1bde7e0fbf5b65123a0b29b74768e24a18c381
Domagoj Licitar <licitar.domagoj@gmail.com>
2024-02-20 17:19:11 +0100

    Created ArticlesController and View for that

---
+++ AbcStore/Controllers/ArticlesController.cs
@@ -0,0 +1,25 @@
+﻿using AbcStore.DbConnect;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AbcStore.Controllers
+{
+    public class ArticlesController : Controller
+    {
+        private AbcStoreContext _abcStoreContext;
+
+        public ArticlesController()
+        {
+            _abcStoreContext = new AbcStoreContext();
+        }
+
+        // GET: Articles
+        public ActionResult Index()
+        {
+            return View(_abcStoreContext.Artikli.ToList());
+        }
+    }
+}

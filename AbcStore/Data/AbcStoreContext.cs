12fb23f096fa51d44408784285ba4a4353182500
Domagoj Licitar <licitar.domagoj@gmail.com>
2024-02-20 21:14:28 +0100

    Added new ArticleController with Views connected to Entity Framework and sql

---
+++ AbcStore/Data/AbcStoreContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace AbcStore.Data
+{
+    public class AbcStoreContext : DbContext
+    {
+        public AbcStoreContext() : base("name=AbcStoreContext")
+        {
+        }
+
+        public System.Data.Entity.DbSet<AbcStore.Models.Article> Articles { get; set; }
+    }
+}
---
+++ AbcStore/Models/Article.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AbcStore.Models
+{
+    public class Article
+    {
+        public int Id { get; set; }
+        public string Naziv { get; set; }
+        public string Kategorija { get; set; }
+        public string Cijena { get; set; }
+    }
+}

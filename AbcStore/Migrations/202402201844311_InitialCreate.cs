6f2dbeae6709903c70a9adbff21891cbda914c1c
Domagoj Licitar <licitar.domagoj@gmail.com>
2024-02-21 14:35:42 +0100

    Enabled migrations for entity framework

---
+++ AbcStore/Migrations/202402201844311_InitialCreate.cs
@@ -0,0 +1,28 @@
+﻿namespace AbcStore.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitialCreate : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Articles",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Naziv = c.String(),
+                        Kategorija = c.String(),
+                        Cijena = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Articles");
+        }
+    }
+}
---
+++ AbcStore/Migrations/Configuration.cs
@@ -0,0 +1,24 @@
+﻿namespace AbcStore.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<AbcStore.Data.AbcStoreContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+            ContextKey = "AbcStore.Data.AbcStoreContext";
+        }
+
+        protected override void Seed(AbcStore.Data.AbcStoreContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
